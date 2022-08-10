bf5693ff23050047129ba32c78ca111199c4c933
Cyberzoneprojects <cyberzone683@gmail.com>
2022-08-10 01:12:23 +0100

    redirect error

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.get("/google", passport.authenticate("google", ["profile", "email"]));
 router.get(
 	"/google/callback",
 	passport.authenticate("google", {
-		successRedirect: "http://localhost:3000/",
+		successRedirect: "/login/success",
 		failureRedirect: "/login/failed",
 	})
 );
